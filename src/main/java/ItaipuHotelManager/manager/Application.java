984575c80dcd9291100cca7b809ff196a2b40bc2
Dutkercz <dutkercz@gmail.com>
2025-02-13 02:15:41 -0300

    Finish...

--- src/main/java/ItaipuHotelManager/manager/Application.java
+++ src/main/java/ItaipuHotelManager/manager/Application.java
@@ -1,12 +1,18 @@
 package ItaipuHotelManager.manager;
 
+import ItaipuHotelManager.manager.frontend.HotelManagementUI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(Application.class, args);
+        Thread.sleep(21000);
+        SwingUtilities.invokeLater(HotelManagementUI::createAndShowGUI);
     }
 }
